08628733dc0053c0418f41f0b8343331b5988343
Xavier <luis.ochoa02@epn.edu.ec>
2026-02-20 19:32:10 -0500

    Update API_URL to include '/api' endpoint

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'https://c1sistema-matriculas.vercel.app'
+const API_URL = 'https://c1sistema-matriculas.vercel.app/api'
 
 const api = axios.create({
   baseURL: API_URL
