6245a01fe5be920bcb5890379b71f79b434ccf50
Xavier <luis.ochoa02@epn.edu.ec>
2026-02-20 19:42:39 -0500

    Add new CORS origin for frontend application

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,6 +18,7 @@ app.use(
   cors({
     origin: [
       'https://c1sistema-matriculas.vercel.app',
+      'https://c1sistema-matriculasfront.vercel.app',
       'http://localhost:5173',
       'http://localhost:3000',
       process.env.URL_FRONTEND,
