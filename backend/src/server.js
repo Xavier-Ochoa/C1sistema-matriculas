c77565f5423c852bfe18acd48435a0a7dc5fb6f8
Xavier <luis.ochoa02@epn.edu.ec>
2026-02-20 19:28:24 -0500

    Update CORS configuration to include new origin

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      'http://127.0.0.1:5501',
+      'https://c1sistema-matriculas.vercel.app',
       'http://localhost:5173',
       'http://localhost:3000',      
       process.env.URL_FRONTEND || "*"
